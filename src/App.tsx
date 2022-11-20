572e62666fe24194643d5be9dc697883e4bbfbe7
xiaobin <newxiaobin@gmail.com>
2022-11-21 02:00:18 +0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 
 import Comp1 from '@/components/Comp1'
+import { Button, Spin } from 'antd';
 
 function App() {
 
   return (
     <div className="App">
       <Comp1 />
+      <Button />
+      <Spin />
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import {
+  createStyleImportPlugin,
+} from 'vite-plugin-style-import'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), createStyleImportPlugin({
+    libs: [
+      // If you don’t have the resolve you need, you can write it directly in the lib, or you can provide us with PR
+      {
+        libraryName: 'antd',
+        esModule: false,
+        resolveStyle: (name) => {
+          return `antd/es/${name}/style/index`
+        },
+      },
+    ]
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   }
 })
+
+
+
+
+
+
+
+
+
